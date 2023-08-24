30c37c62d83d13362728b0c684e02aeb4006c438
Sasanka <sajewsasanka1@gmail.com>
2023-08-24 12:30:45 +0530

    Basic Details Added

---
+++ frontend/src/test/java/me/moody/frontend/FrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.moody.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
